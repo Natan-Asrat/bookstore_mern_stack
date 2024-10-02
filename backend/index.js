400b4e5439fa21067ffbefad0c725b35a2aadf33
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-10-02 20:56:42 +0300

    post books

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+import express from "express";
+
+import {PORT, mongoDBURL} from "./config.js"
+import mongoose from 'mongoose';
+import {Book} from './models/bookModel.js'
+const app = express();
+
+app.get("/", (request, response) => {
+    console.log(request)
+    return response.status(234).send("Welcome to MERN Stack")
+});
+
+app.post('/books', async (request, response) => {
+    try {
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: 'Send all required fields'
+            })
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear
+        }
+    }catch (error){
+        console.log(error)
+        response.status(500).send({message: error.message})
+    }
+})
+
+mongoose.connect(
+    mongoDBURL
+).then(()=>{
+    console.log("App connected to database")
+    app.listen(PORT, ()=> {
+        console.log(`App is listening to port: ${PORT}`)
+    })
+}).catch((error)=>{
+    console.log(error);
+    alert("DB error");
+})
