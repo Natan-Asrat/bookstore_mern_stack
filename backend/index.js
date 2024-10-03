f29fa29684512997383620ed181f164fae94b761
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-10-03 17:26:14 +0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import cors from 'cors';
 const app = express();
 app.use(express.json())
 app.use(cors({
-    origin: 'https://natans-bookstore.vercel.app/',
+    origin: 'https://natans-bookstore.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type']
 }))
