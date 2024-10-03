d06fef8793836bd4a984fce55f8b94b8db5ace1a
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-10-03 08:20:15 +0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,15 @@ import bookRoutes from './routes/bookRoutes.js';
 
 import {PORT, mongoDBURL} from "./config.js"
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}))
 app.get("/", (request, response) => {
     console.log(request)
     return response.status(234).send("Welcome to MERN Stack")
