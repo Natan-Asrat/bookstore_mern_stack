7e910a9568fbb4278a56472e8bd19aa3150edcf1
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-10-03 07:14:31 +0300

    crud

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import {PORT, mongoDBURL} from "./config.js"
 import mongoose from 'mongoose';
 import {Book} from './models/bookModel.js'
 const app = express();
-
+app.use(express.json())
 app.get("/", (request, response) => {
     console.log(request)
     return response.status(234).send("Welcome to MERN Stack")
@@ -26,9 +26,67 @@ app.post('/books', async (request, response) => {
             author: request.body.author,
             publishYear: request.body.publishYear
         }
+        const book = await Book.create(newBook)
+        return response.status(201).send(book);
     }catch (error){
         console.log(error)
         response.status(500).send({message: error.message})
+    }
+})
+
+app.get("/books", async (request, response) => {
+    try{
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        })
+    } catch (error){
+        console.log(error)
+        response.status(500).send({message: error.message})
+    }
+})
+
+app.get("/books/:id", async (request, response)=> {
+    try{
+        const {id} = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book)
+    }catch(error){
+        console.log(error)
+        return response.status(500).send(error.message)
+    }
+})
+
+app.put("/books/:id", async (request, response) => {
+    try{
+        const {id} = request.params;
+        const book = await Book.findByIdAndUpdate(id, request.body,{ new: true });
+        if(!book){
+            return response.status(404).json({message: "Book not found"})
+        }
+        return response.status(200).json(
+            book
+        )
+    } catch (error) {
+        console.log(error);
+        return response.status(500).send({message: error.message})
+    }
+})
+
+app.delete("/books/:id", async (request, response) => {
+    try{
+        const {id} = request.params;
+        const result = await Book.findByIdAndDelete(id);
+
+        if(!result){
+            return response.status(404).json({message: "Book not found"});
+        }
+        return response.status(200).send(result)
+
+    }catch(error){
+        console.log(error)
+        return response.status(500).send({message: error.message})
     }
 })
 
@@ -41,5 +99,4 @@ mongoose.connect(
     })
 }).catch((error)=>{
     console.log(error);
-    alert("DB error");
 })
