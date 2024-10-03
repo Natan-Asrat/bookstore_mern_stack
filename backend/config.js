1fe76eaa252dab3031e01c232905c5754fb0e8e7
Natan-Asrat <nathanyilmaasrat@gmail.com>
2024-10-03 17:17:27 +0300

    version

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
-export const PORT = 5555;
+export const PORT = 8080;
 export const mongoDBURL =  "mongodb+srv://root:natan@bookstore.m4ocy.mongodb.net/?retryWrites=true&w=majority&appName=bookstore";
